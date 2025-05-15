8d16e15a74c537412503fc9f28c19b84119afc39
Grigo471 <grigo471@gmail.com>
2025-05-15 14:00:22 +0300

    fixed production build

--- README.md
+++ README.md
@@ -18,7 +18,7 @@ Install dependencies:
 npm install
 ```
 
-### Build
+### Build in dev mode
 
 Build and start the project:
 
@@ -27,6 +27,16 @@ npm run start
 ```
 
 The application will be served and can be accessed at [http://localhost:3000](http://localhost:3000).
+
+### Build in prod mode
+
+Build project in production mode:
+
+```bash
+npm run build
+```
+
+The application files will be generated in ./dist folder.
 
 
 ## Information
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,14 +1,28 @@
-// SPDX-FileCopyrightText: NOI Techpark <digital@noi.bz.it>
-//
-// SPDX-License-Identifier: CC0-1.0
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
 const path = require('path');
 
 module.exports = {
   mode: 'production',
   entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html'),
+      filename: 'index.html'
+    }),
+    new CleanWebpackPlugin(),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'public', 'styles'),
+          to: path.resolve(__dirname, 'dist', 'styles'),
+        }
+      ]
+    })
+  ],
   output: {
-    filename: 'index.min.js',
+    filename: 'index.js',
     path: path.resolve(__dirname, 'dist'),
   },
 };
